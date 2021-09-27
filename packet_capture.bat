457e5b696d698af81fe7a3d29df502dd59ab747e
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-09-27 22:53:10 +0900

    Update packet_capture.bat

--- packet_capture.bat
+++ packet_capture.bat
@@ -1,30 +1,31 @@
 @echo off
 
+chcp 65001 
+@rem SHIFT-JISからUTF-8に変換
+
 @rem デフォルトの格納場所を指定
-set main_dir = D:\Documents\GitHub\packet-capture\
+set main_dir=D:\Documents\GitHub\packet-capture\
 
-@rem ログのパケット名を作成
-set today = %date:~0,4%%date:~5,2%%date:~8,2%
-@rem todayにdateコマンドの ” / ” 抜きを代入
+@rem フォルダにキャプチャした日付をつける
+set today=%date:~0,4%%date:~5,2%%date:~8,2%
+set sub_dir=%main_dir%%today%
+mkdir %sub_dir%
 
-set sub_dir = %main_dir%%today%
-makdir %sub_dir%
-
+@echo.
 @rem キャプチャするインターフェイスを表示させる
 @echo ネットワークインターフェイス一覧
 @echo.
-"C:\Program Files\Wireshark\dumpcap.exe" -Desktop
+@echo ===============================================================================
+"C:\Program Files\Wireshark\dumpcap.exe" -D
+@echo ===============================================================================
 @echo.
 
-@rem キャプチャするインターフェイス情報を取得
-set /p interface_id = "キャプチャしたいインターフェイスの数字を入力してください :"
+set /p interface_id="キャプチャしたいネットワークインターフェイスの番号を入力してください："
 @echo.
 
-@rem 保存間隔を分単位で指定
-set /p time_interval="保存間隔を分単位で指定してください :"
+set /p time_interval="保存の間隔を分単位で入力してください："
 set /a "time_interval *= 60"
 @echo.
 
-@rem パケットのキャプチャを開始
-@echo キャプチャしたパケットを保存しています。Ctrl＋Cで終了します。
-"C:\Program Files\Wireshark\dumpcap.exe" -i %interface_id% -b duration:%time_interval% -w %sub_dir%\packet.pcapng"
+@echo Wiresharkでパケットを保存します。Ctrl+Cで終了。
+"C:\Program Files\Wireshark\dumpcap.exe" -i %interface_id% -b duration:%time_interval% -w %sub_dir%\packet.pcapng
